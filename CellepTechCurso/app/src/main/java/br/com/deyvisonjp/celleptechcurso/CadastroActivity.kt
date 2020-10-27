9374511298c50e845b88b6e6cb20029ad099c747
Deyvison <deyvisonjp@id.uff.br>
2020-10-26 23:04:53 -0300

    Configurando Login e criando tela de Cadastrro

---
+++ CellepTechCurso/app/src/main/java/br/com/deyvisonjp/celleptechcurso/CadastroActivity.kt
@@ -0,0 +1,84 @@
+package br.com.deyvisonjp.celleptechcurso
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        // Otendo as referencias para os componentes
+        val edtCadastroNome: EditText = findViewById(R.id.edtCadastroNome);
+        val edtCadastroSobrenome: EditText = findViewById(R.id.edtCadastroSobrenome);
+        val edtCadastroEmail: EditText = findViewById(R.id.edtCadastroEmail);
+        val edtCadastroSenha: EditText = findViewById(R.id.edtCadastroSenha);
+        val btnCadastroCdastrar: Button = findViewById(R.id.btnCadastroCadastrar)
+        val spnCadastroSpinner: Spinner = findViewById(R.id.spnCadastroGenero);
+
+        // Criando uma lista de generos para o spineer
+        val listaDeGeneros = arrayListOf("Selecione o genêro", "Feminino", "Masculino", "Não-binário")
+
+        // Criar num adaptador para o spinner
+        val generoAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item, //Layout
+            listaDeGeneros
+        )
+
+        //Plugar a lista Adapter
+        spnCadastroSpinner.adapter = generoAdapter;
+
+        // Listener do botao cadastrar
+        btnCadastroCdastrar.setOnClickListener {
+            // Obtendo os dados inseridos pelo usuário
+            val nome = edtCadastroNome.text.toString().trim();
+            val sobrenome = edtCadastroSobrenome.text.toString().trim();
+            val email = edtCadastroEmail.text.toString().trim();
+            val senha = edtCadastroSenha.text.toString().trim();
+            // Obtendo o item que foi selecionado pelo spinner
+            val genero = spnCadastroSpinner.selectedItem.toString();
+
+            // Validando Campos
+            if (nome.isEmpty() || sobrenome.isEmpty() || email.isEmpty() ||
+                senha.isEmpty() || genero == listaDeGeneros[0]) {
+                // Exibindo a mensagem de erro com Toast
+                Toast.makeText(this, "Todos os campos devem ser preenchidos!", Toast.LENGTH_LONG).show()
+            } else {
+
+                // Criar ou acessar o arquivo de SharedPreferences
+                val sharedPrefs = getSharedPreferences("cadastro_$email",
+                                                        Context.MODE_PRIVATE);
+
+                // Criando uma referencia para o editor do arquivo
+                val editPrefs = sharedPrefs.edit();
+
+                // Definindo as alterações do arquivo
+                editPrefs.putString("NOME", nome)
+                editPrefs.putString("SOBRENOME", sobrenome)
+                editPrefs.putString("EMAIL", email)
+                editPrefs.putString("SENHA", senha)
+                editPrefs.putString("GENERO", genero)
+
+                // Salvando as alterações no arquivo de SharedPreferences
+                editPrefs.apply()
+
+                // Abrir a tela Main após salvar
+                val mIntent = Intent(this, MainActivity::class.java)
+
+                // Passando dados de um activity para outra utlizando a intent
+                mIntent.putExtra("INTENT_EMAIL", email)
+
+                startActivity(mIntent)
+
+                // Fechar todas telas que estavam abertas por dependência a essa
+                finishAffinity()
+
+            }
+        }
+
+    }
+}
--- CellepTechCurso/app/src/main/java/br/com/deyvisonjp/celleptechcurso/LoginActivity.kt
+++ CellepTechCurso/app/src/main/java/br/com/deyvisonjp/celleptechcurso/LoginActivity.kt
@@ -1,13 +1,65 @@
 package br.com.deyvisonjp.celleptechcurso
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Capturando as referências para os copmponentes visuais
+        val editTextEmail: EditText = findViewById(R.id.editTextEmail);
+        val editTextSenha: EditText = findViewById(R.id.editTextSenha);
+        val btnLoginEntrar: Button = findViewById(R.id.btnLoginEntrar);
+        val btnLoginCadastrar: Button = findViewById(R.id.btnLoginCadastrar);
 
+        // Criando o Listener (ouvinte) do botao
+        btnLoginEntrar.setOnClickListener {
+            val email = editTextEmail.text.toString().trim();
+            val senha = editTextSenha.text.toString().trim();
+
+            // Validação dos campos
+            if (email.isEmpty()) {   // o mesmo que -> if (email == " ")
+                editTextEmail.error = "Campo obrigatório"
+                editTextEmail.requestFocus()
+            }
+
+            if (senha.isEmpty()) {
+                editTextSenha.error = "Campo obrigatório"
+                editTextSenha.requestFocus()
+            }
+
+            else { //Campos não estão vazios
+                // Verificação de email e senha
+                if (email == "teste@teste.com" && senha == "123456") {
+
+                    // Exinindo uma mensagem usando o Toast -> makeText (tela a mostrar, texto a ser exibido, tempo a ser exibido)
+                    Toast.makeText(this, "Você acaba de logar . . .", Toast.LENGTH_SHORT).show()
+
+                    // Abrir MainActivity
+                    val mIntent = Intent(
+                        this,
+                        MainActivity::class.java
+                    ) // Intent(Tela que estamos, tela a ser direcionada)
+
+                    startActivity(mIntent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(this, "Email ou senha inválidos", Toast.LENGTH_SHORT).show()
+                }
+            }
+        };
+
+        // Listener do botao cadastrar
+        btnLoginCadastrar.setOnClickListener {
+            val mIntent = Intent(this, CadastroActivity::class.java)
+            startActivity(mIntent)
+        }
     }
 }
